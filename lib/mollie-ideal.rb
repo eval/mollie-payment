2c17b525cb69f87565988f5e5717f146daeb4e2d
Gert Goet <gert@thinkcreate.nl>
2011-10-12 21:45:44 +0200

    bundle gem

---
+++ lib/mollie-ideal.rb
@@ -0,0 +1,7 @@
+require "mollie-ideal/version"
+
+module Mollie
+  module Ideal
+    # Your code goes here...
+  end
+end
---
+++ lib/mollie-ideal/version.rb
@@ -0,0 +1,5 @@
+module Mollie
+  module Ideal
+    VERSION = "0.0.1"
+  end
+end
